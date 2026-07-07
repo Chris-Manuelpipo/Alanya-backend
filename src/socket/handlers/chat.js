98f9dc33de6ea2857aa212d62e34ebdc5402ff95
chris <etchomechris2000@gmail.com>
2026-07-08 00:04:09 +0100

    fix(notifications)

--- src/socket/handlers/chat.js
+++ src/socket/handlers/chat.js
@@ -1,7 +1,6 @@
 const pool = require('../../config/db');
 const { evaluateDirectMessageSend, shouldSuppressDirectInteraction, isBlockedBy, getDirectConversationPeer, emitPresenceUpdate } = require('../../utils/blockUtils');
 const { resolveLastMessagePreview } = require('../../utils/mediaAlbum');
-const { resolveReplyToID } = require('../../utils/messageReply');
 const { resolveReplyToID } = require('../../utils/resolveReplyToID');
 
 const joinConversation = (io, socket, userSockets) => {
@@ -45,7 +44,7 @@ const messageSend = (io, socket, userSockets) => {
       }
       const silentDrop = blockEval.isDirect && blockEval.action === 'silent';
 
-      const resolvedReplyToID = await resolveReplyToID(pool, replyToID, conversationID);
+      const resolvedReplyToID = await resolveReplyToID(conversationID, replyToID);
       const resolvedReplyToContent = resolvedReplyToID != null ? (replyToContent ?? null) : null;
 
       // ÉTAPE 1 : PERSISTER le message en DB
